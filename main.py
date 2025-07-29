25b58651a9809f5882ae11abf5e1ddafadc7bbd1
Kavya Rambhia <161142013+kavya-r30@users.noreply.github.com>
2025-07-29 18:00:53 +0530

    2.5 lite

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ GENAI_API_KEY = os.getenv("GENAI_API_KEY")
 EXPECTED_TOKEN = os.getenv("EXPECTED_TOKEN")
 
 genai.configure(api_key=GENAI_API_KEY)
-model = genai.GenerativeModel("gemini-2.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash-lite")
 
 app = FastAPI()
 
